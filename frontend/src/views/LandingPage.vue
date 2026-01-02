af0be4b2ca1a4946b9f03b991682411e422652cf
aceecahub <chaliaforever1@gmail.com>
2026-01-02 10:04:35 +0700

    progress Contact Section

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -3,10 +3,14 @@ import NavbarList from "../components/landingPage/NavbarList.vue"
 import WelcomeSection from "../components/landingPage/WelcomeSection.vue"
 import AboutSection from "../components/landingPage/AboutSection.vue"
 import GalerySection from "../components/landingPage/GalerySection.vue"
+import ContactSection from "../components/landingPage/ContactSection.vue"
 </script>
  <template>
+   <div style="font-family: playfair-display">
     <NavbarList />
     <WelcomeSection />
     <AboutSection />
     <GalerySection />
+    <ContactSection />
+   </div>
  </template>
